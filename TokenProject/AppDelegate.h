8196e14dd1fa09d44686f0e6e8b99fdde3b5c744
liujianting <liujianting@qiyi.com>
2018-03-29 11:33:39 +0800

    Initial Commit

---
+++ TokenProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TokenProject
+//
+//  Created by 刘建廷 on 2018/3/29.
+//  Copyright © 2018年 刘建廷. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
